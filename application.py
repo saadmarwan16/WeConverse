9c8d532ea95b86a720effec1f3bcad47971e34e1
Marwan <saadmarwan16@gmail.com>
2020-12-15 06:54:01 +0000

    Created the login and sign-up pages

--- application.py
+++ application.py
@@ -1,13 +1,78 @@
 import os
+import requests
 
-from flask import Flask
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
 from flask_socketio import SocketIO, emit
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
+from modules.helpers import login_required
+
+# Configure application
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+# app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+# db = SQL("sqlite:///finance.db")
 
 
 @app.route("/")
+@login_required
 def index():
-    return "Project 2: TODO"
+    # Forget any user_id
+    session.clear()
+
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    return render_template("login.html")
+
+
+@app.route("/sign-up")
+def sign_up():
+    return render_template("sign-up.html")
+
+
+@app.route("/history")
+@login_required
+def history():
+    return render_template("history.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
